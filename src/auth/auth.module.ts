e6351cd69300e0883f44406bc5346045dca60af9
Vanshiljain <115866666+Vanshiljain@users.noreply.github.com>
2024-08-05 11:29:05 +0530

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthResolver } from './auth.resolver';
+import { AuthResolver } from './auth.resolver'; 
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt'; 
 import { jwtConstants } from './constants';
